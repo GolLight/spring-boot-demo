e786e7f5770e1a401b8cc006564702da47205fa3
gollight <gollight12138@gmail.com>
2023-03-05 10:59:36 +0800

    remove unused

--- src/test/java/com/gollight/tacocloud/HomeControllerTests.java
+++ src/test/java/com/gollight/tacocloud/HomeControllerTests.java
@@ -1,14 +1,11 @@
 package com.gollight.tacocloud;
 
-import static org.mockito.ArgumentMatchers.contains;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 @WebMvcTest(HomeController.class)
 public class HomeControllerTests {
